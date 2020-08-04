7973edbe8fc31abb31a235689ad42862e34baf0f
unknown <alejandrojzeballos@gmail.com>
2020-08-04 02:17:34 -0400

    añadiendo introduccion a support IT

--- inicio.js
+++ inicio.js
@@ -1,1 +1,14 @@
 /* comenzando con los cambios */
+/* Introduccion
+Que es IT
+Las computadoras son el centro de todo en estos, la herramienta mas usada ahora 
+IT (information Technology)
+Es el uso de tecnologia como computadoras e internet, para almacenar y procesar datos en informacion muy util.
+con esto nos referimos a todas las ramas de IT como redes pero cuando nos referimos a support es con la interaccion con la gente.
+Digital divide es la brecha digital, personas que no tinen acceso a internet o a computadoras.
+
+los support It ayudan a que esta brecha digital sea mas pequeña por que introduciomos y ayudamos a que puedan manejar mejor las tecnologias.
+¿que hacen los suppport it dia a dia?
+se puede describir el trabajo diario como manejo,instalacion,mantenimiento,solucionar problemas y configuracion.
+
+*/
